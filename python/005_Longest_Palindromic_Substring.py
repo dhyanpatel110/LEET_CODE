03ff9f115a736a824fc25db308ada8d82eb9ec1e
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-04 20:02:36 +0530

    Create 005_Longest_Palindromic_Substring.py

---
+++ python/005_Longest_Palindromic_Substring.py
@@ -0,0 +1,46 @@
+Example 1:
+
+Input: s = "babad"
+Output: "bab" = 3
+Note: "aba" is also a valid answer
+  
+Example 2:
+
+Input: s = "baddad"
+Output: "adda" = 4
+
+#CODE:
+  class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        resLen = 0
+        
+        for i in range (len(s)):
+            #odd length
+            l = i
+            r = i
+            
+            while l>=0 and r<len(s) and s[l] == s[r]:
+                if(r-l+1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r-l+1
+                
+                l -= 1
+                r += 1
+                
+            #even length
+            l = i
+            r = i+1
+            
+            while l>=0 and r<len(s) and s[l] == s[r]:
+                if(r-l+1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r-l+1
+                
+                l -= 1
+                r += 1
+        return res
+ 
+
+
+
