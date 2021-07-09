91ce45f0939e92eca1f07c734bcc304cc64d3579
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-09 22:37:21 +0530

    Create 009_Palindrome_Number.py

---
+++ python/009_Palindrome_Number.py
@@ -0,0 +1,13 @@
+Example 1:
+  Input: x = 121
+  Output: true
+    
+Example 2:
+  Input: x = 10
+  Output: false
+  Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+CODE:
+  class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return (str(x) == str(x)[::-1])
