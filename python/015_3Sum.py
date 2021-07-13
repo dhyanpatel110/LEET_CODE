62a72fbd946a510774b8ae399deab72283b2beee
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-13 22:44:15 +0530

    Create 015_3Sum.py

---
+++ python/015_3Sum.py
@@ -0,0 +1,34 @@
+Example 1:
+  Input: nums = [-1,0,1,2,-1,-4]
+  Output: [[-1,-1,2],[-1,0,1]]
+
+CODE:
+  class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+        
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
+                continue
+                
+            l = i + 1
+            r = len(nums)-1
+                
+            while l < r:
+                threesum = a + nums[l] + nums[r] 
+                if threesum > 0:
+                    r -= 1
+                elif threesum < 0:
+                    l += 1
+                else:
+                    result.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+        return result
+      
+      
+      
+      
+      
