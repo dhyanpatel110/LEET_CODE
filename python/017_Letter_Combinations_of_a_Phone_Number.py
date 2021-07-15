62b643012f63b175efa6093ceb89a90db93313dd
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-15 22:39:27 +0530

    Create 017_Letter_Combinations_of_a_Phone_Number.py

---
+++ python/017_Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,29 @@
+Example 1:
+  Input: digits = "23"
+  Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+CODE:
+  class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        phone_map = {"2" : "abc",
+                     "3" : "def",
+                     "4" : "ghi",
+                     "5" : "jkl",
+                     "6" : "mno",
+                     "7" : "pqrs",
+                     "8" : "tuv",
+                     "9" : "wxyz" }
+        
+        if digits == "":
+            return []
+        
+        numbers = list(phone_map[digits[0]])
+        
+        for digit in digits[1:]:
+            numbers = [old+new for old in numbers for new in list(phone_map[digit])]
+        return numbers
+        
+        
+        
+        
+        
