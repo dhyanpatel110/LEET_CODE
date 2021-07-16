0598c7cf28b19dec491554a22d4431002deaf391
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-16 22:22:45 +0530

    Update 018_4Sum.py

--- python/018_4Sum.py
+++ python/018_4Sum.py
@@ -9,10 +9,12 @@ Example 2:
 CODE:
   class Solution(object):
     def fourSum(self, nums, target):
+      
         sort_nums = sorted(nums)
         ls = len(nums)
         res = {}
         pairs = {}
+        
         for i in range(ls - 3):
             for j in range(i + 1, ls - 2):
                 res_2 = sort_nums[i] + sort_nums[j]
@@ -20,10 +22,12 @@ CODE:
                     pairs[target - res_2].append([i, j])
                 except KeyError:
                     pairs[target - res_2] = [[i, j]]
+                    
         for key, temp in pairs.items():
             for pair in temp:
                 j = pair[1] + 1
                 k = ls - 1
+                
                 while j < k:
                     current = sort_nums[j] + sort_nums[k]
                     if current == key:
