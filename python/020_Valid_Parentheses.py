be36304a05d23ead3a9469201911fec627051bb1
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-17 18:42:58 +0530

    Create 020_Valid_Parentheses.py

---
+++ python/020_Valid_Parentheses.py
@@ -0,0 +1,30 @@
+"""
+Example 1:
+  Input: s = "()[]{}"
+  Output: true
+    
+Example 2:
+  Input: s = "(]"
+  Output: false
+    
+Example 3:
+  Input: s = "{[]}"
+  Output: true
+"""
+    
+CODE:
+  class Solution:
+    def isValid(self, s: str) -> bool:
+        replace = True
+        while replace:
+            start_length = len(s)
+            for inner in ["{}","()","[]"]:
+                s = s.replace(inner,"")
+            if start_length == len(s):
+                replace = False
+        return s ==""
+      
+      
+      
+      
+      
