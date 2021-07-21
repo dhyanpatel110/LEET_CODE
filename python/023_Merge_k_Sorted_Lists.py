ed6ecf7becb0a7f8c276fd3467086fd8e5b2748c
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-21 22:38:02 +0530

    Create 023_Merge_k_Sorted_Lists.py

---
+++ python/023_Merge_k_Sorted_Lists.py
@@ -0,0 +1,34 @@
+'''
+Example 1:
+  Input: lists = [[1,4,5],[1,3,4],[2,6]]
+  Output: [1,1,2,3,4,4,5,6]
+  Explanation: The linked-lists are:
+  [
+     1->4->5,
+     1->3->4,
+     2->6
+  ]
+  merging them into one sorted list:
+  1->1->2->3->4->4->5->6
+'''
+
+CODE:
+  class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        self.node = []
+        dummy = head = ListNode(0)
+        
+        for l in lists:
+            while l:
+                self.node.append(l.val)
+                l = l.next
+                
+        for x in sorted(self.node):
+            dummy.next = ListNode(x)
+            dummy = dummy.next
+        return head.next
+      
+      
+      
+      
+      
