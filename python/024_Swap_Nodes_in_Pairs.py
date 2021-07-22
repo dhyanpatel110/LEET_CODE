7bfb033a2c83a558f3d5ea10cb0ebfb77b8c729d
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-22 21:45:36 +0530

    Update 024_Swap_Nodes_in_Pairs.py

--- python/024_Swap_Nodes_in_Pairs.py
+++ python/024_Swap_Nodes_in_Pairs.py
@@ -2,12 +2,12 @@
 Example 1:
   Input: head = [1,2,3,4]
   Output: [2,1,4,3]
-  
-  | col 1      | col 2      |
+'''
+| col 1      | col 2      |
 |------------|-------------|
 | <img src="https://media.wired.com/photos/5926db217034dc5f91becd6b/master/w_582,c_limit/so-logo-s.jpg" width="250"> | <img src="https://mk0jobadderjftub56m0.kinstacdn.com/wp-content/uploads/stackoverflow.com-300.jpg" width="250"> |
-'''
-ODE:
+
+CODE:
   class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
         d1 = d = ListNode(0)
