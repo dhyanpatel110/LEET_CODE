a7987d2f1d43cffb78bd7d96d995d44b6dbc489a
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-07-25 15:55:37 +0530

    Create 028_Implement_strStr.py

---
+++ python/028_Implement_strStr.py
@@ -0,0 +1,28 @@
+'''
+Example 1:
+  Input: haystack = "hello", needle = "ll"
+  Output: 2
+
+Example 2:
+  Input: haystack = "aaaaa", needle = "bba"
+  Output: -1
+    
+Example 3:
+  Input: haystack = "", needle = ""
+  Output: 0
+'''
+
+CODE:
+  class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) == 0:
+            return 0
+        for i in range(len(haystack)):
+            if haystack[i : i+len(needle)] == needle:
+                return i
+        return -1
+      
+      
+      
+      
+      
