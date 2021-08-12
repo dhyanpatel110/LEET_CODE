ec8578b3f0dba6306b84d3ee2b8d04dec827ed0d
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-08-12 22:52:21 +0530

    Create 047_Permutations_II.py

---
+++ python/047_Permutations_II.py
@@ -0,0 +1,45 @@
+'''
+Example 1:
+  Input: nums = [1,1,2]
+  Output:
+  [[1,1,2],
+  [1,2,1],
+  [2,1,1]]
+  
+Example 2:
+  Input: nums = [1,2,3]
+  Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+'''
+
+#CODE:
+  class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        perm = []
+        count = { n:0 for n in nums }
+        
+        for n in nums:
+            count[n] += 1
+        
+        def dfs():
+            if len(perm) == len(nums):
+                res.append(perm.copy())
+                return
+            
+            for n in count:
+                if count[n] > 0:
+                    perm.append(n)
+                    count[n] -= 1
+                    
+                    dfs()
+                    
+                    count[n] += 1
+                    perm.pop()
+                    
+        dfs()
+        return res
+                
+        
+
+        
+        
