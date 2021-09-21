46b4c37205e3fa90e94667e48e5bb47a366a75b1
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-09-21 21:02:57 +0530

    Create 056_Merge_Intervals.py

---
+++ python/056_Merge_Intervals.py
@@ -0,0 +1,31 @@
+'''
+Example 1:
+  Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+  Output: [[1,6],[8,10],[15,18]]
+  Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+    
+Example 2:
+  Input: intervals = [[1,4],[4,5]]
+  Output: [[1,5]]
+  Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+'''
+
+CODE:
+  class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda i: i[0])
+        output = [intervals[0]]
+        
+        for start, end in intervals[1:]:
+            lastEnd = output[-1][1]
+            
+            if start <= lastEnd:
+                output[-1][1] = max(lastEnd, end)
+            else:
+                output.append([start, end])
+        return output
+                
+        
+        
+        
+        
