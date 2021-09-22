e18a72f99e7bca36af2cc7e9dd18459c2237bec0
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-09-22 21:26:11 +0530

    Create 057_Insert_Interval.py

---
+++ python/057_Insert_Interval.py
@@ -0,0 +1,44 @@
+'''
+Example 1:
+  Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+  Output: [[1,5],[6,9]]
+  
+Example 2:
+  Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+  Output: [[1,2],[3,10],[12,16]]
+  Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+  
+Example 3:
+  Input: intervals = [], newInterval = [5,7]
+  Output: [[5,7]]
+  
+Example 4:
+  Input: intervals = [[1,5]], newInterval = [2,3]
+  Output: [[1,5]]
+  
+Example 5:
+  Input: intervals = [[1,5]], newInterval = [2,7]
+  Output: [[1,7]]
+'''
+
+#CODE:
+  class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        
+        for i in range (len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1]) ]
+        
+        res.append(newInterval)
+        return res
+        
+        
+        
+        
+        
