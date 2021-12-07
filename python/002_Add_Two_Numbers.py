ba302ce1cf564278412906e3bb9a94471a28d787
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-12-07 21:02:56 +0530

    Update 002_Add_Two_Numbers.py

--- python/002_Add_Two_Numbers.py
+++ python/002_Add_Two_Numbers.py
@@ -1,6 +1,9 @@
-#Input: l1 = [2,4,3], l2 = [5,6,4]
-#Output: [7,0,8]
-#Explanation: 342 + 465 = 807.
+'''
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807
+'''
+
 #CODE:
   # Definition for singly-linked list.
   # class ListNode:
