b29e2d0f7aa4e2380040a32300d10edbd219f92b
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-12-21 21:01:41 +0530

    Update 036_Valid_Sudoku.py

--- python/036_Valid_Sudoku.py
+++ python/036_Valid_Sudoku.py
@@ -27,7 +27,7 @@ Example 2:
   Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
 '''
 
-CODE:
+#CODE:
   class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         row = set()
