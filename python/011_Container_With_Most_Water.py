52d1b0f53e175c561695398acf12cf9d77fc22d0
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-01-02 23:00:18 +0530

    Update 011_Container_With_Most_Water.py

--- python/011_Container_With_Most_Water.py
+++ python/011_Container_With_Most_Water.py
@@ -3,7 +3,7 @@ Example 1:
   Input: height = [1,8,6,2,5,4,8,3,7]
   Output: 49
   Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
-
+'''
 #CODE:
   class Solution:
     def maxArea(self, height: List[int]) -> int:
