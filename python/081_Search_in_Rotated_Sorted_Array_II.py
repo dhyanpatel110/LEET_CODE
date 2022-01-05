b00db7b65c3b1cc6ec2e31488e380d2f09914b54
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-01-05 22:06:57 +0530

    Create 081_Search_in_Rotated_Sorted_Array_II.py

---
+++ python/081_Search_in_Rotated_Sorted_Array_II.py
@@ -0,0 +1,15 @@
+Example 1:
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+
+Example 2:
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+
+#CODE
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return True
+        return False
