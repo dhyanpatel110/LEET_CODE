29706f7e3e7140611f2cc6d49489d3f5d8be263a
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-01-15 21:01:24 +0530

    Update 086_Partition_List.py

--- python/086_Partition_List.py
+++ python/086_Partition_List.py
@@ -1,3 +1,4 @@
+'''
 Example 1:
 Input: head = [1,4,3,2,5,2], x = 3
 Output: [1,2,2,4,3,5]
@@ -5,7 +6,8 @@ Output: [1,2,2,4,3,5]
 Example 2:
 Input: head = [2,1], x = 2
 Output: [1,2]
-  
+''' 
+
 #CODE:
   # Definition for singly-linked list.
   # class ListNode:
