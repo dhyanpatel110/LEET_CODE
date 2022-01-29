4feaccf6c839fa9610ff3002dd67290d5a907d74
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-01-29 21:01:20 +0530

    Update 103_Binary_Tree_Zigzag_Level_Order_Traversal.py

--- python/103_Binary_Tree_Zigzag_Level_Order_Traversal.py
+++ python/103_Binary_Tree_Zigzag_Level_Order_Traversal.py
@@ -1,3 +1,4 @@
+'''
 Example 1:
             3
            / \
@@ -15,7 +16,8 @@ Output: [[1]]
 Example 3:
 Input: root = []
 Output: []
-  
+'''
+
 #CODE:
   # Definition for a binary tree node.
   # class TreeNode:
