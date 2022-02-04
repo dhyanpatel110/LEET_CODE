8294d332c82be02868c6c13a98a6df8c8c21181d
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-02-04 16:54:43 +0530

    Create 112_Path_Sum.py

---
+++ python/112_Path_Sum.py
@@ -0,0 +1,57 @@
+Example 1:
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1   
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+
+Example 2:
+  
+               1
+              / \
+             2   3
+
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+
+Example 3:
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+  
+#CODE:
+  # Definition for a binary tree node.
+  # class TreeNode:
+  #     def __init__(self, val=0, left=None, right=None):
+  #         self.val = val
+  #         self.left = left
+  #         self.right = right
+  class Solution:
+      def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+          def dfs(node, curSum):
+              if not node:
+                  return False
+            
+              curSum += node.val
+              if not node.left and not node.right:
+                  return curSum == targetSum
+            
+              return(dfs(node.left, curSum) or dfs(node.right, curSum))
+        
+          return dfs(root, 0)
+                
+          
+          
+          
+          
