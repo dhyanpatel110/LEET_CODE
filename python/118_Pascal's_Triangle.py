895220be140b07cb7f039f54d5cf46565b2334ae
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-02-07 17:59:43 +0530

    Create 118_Pascal's_Triangle.py

---
+++ python/118_Pascal's_Triangle.py
@@ -0,0 +1,28 @@
+'''
+Example 1:
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+Example 2:
+Input: numRows = 1
+Output: [[1]]
+'''
+
+#CODE:
+  class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+        
+        for i in range(numRows - 1):
+            temp = [0] + res[-1] + [0]
+            row = []
+            for j in range(len(res[-1]) + 1):
+                row.append(temp[j] + temp[j+1])
+            res.append(row)
+        return res
+        
+        
+        
+        
+        
+        
