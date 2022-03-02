c7538676e54f79b85b3e900e6478eb1873771f54
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-03-02 22:28:26 +0530

    Update 080_Remove_Duplicates_from_Sorted_Array_II.py

--- python/080_Remove_Duplicates_from_Sorted_Array_II.py
+++ python/080_Remove_Duplicates_from_Sorted_Array_II.py
@@ -1,3 +1,4 @@
+'''
 Example 1:
 Input: nums = [1,1,1,2,2,3]
 Output: 5, nums = [1,1,2,2,3,_]
@@ -9,6 +10,7 @@ Input: nums = [0,0,1,1,1,1,2,3,3]
 Output: 7, nums = [0,0,1,1,2,3,3,_,_]
 Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores).
+'''
 
 #CODE:
 class Solution:
