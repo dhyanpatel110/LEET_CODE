a9eea6321d7d03a85df4100fc4b9f282f6c81809
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-04-17 13:42:14 +0530

    Create 148_Sort_List.py

---
+++ python/148_Sort_List.py
@@ -0,0 +1,45 @@
+'''
+Example 1:
+ 4 => 2 => 1 => 3
+        |
+ 1 => 2 => 3 => 4    
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+
+Example 2:
+ -1 => 5 => 3 => 4 => 0
+            |
+ -1 => 0 => 3 => 4 => 5
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+
+Example 3:
+Input: head = []
+Output: []
+'''
+
+CODE:
+  # Definition for singly-linked list.
+  # class ListNode:
+  #     def __init__(self, val=0, next=None):
+  #         self.val = val
+  #         self.next = next
+  class Solution:
+     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+          if not head:
+              return
+          current = head
+          values = []
+          while current:
+              values.append(current.val)
+              current = current.next
+          values.sort(reverse=True)
+          head = ListNode(values.pop())
+          node = head
+          while values:
+              node.next = ListNode(values.pop())
+              node = node.next
+          return head
+        
+        
+        
