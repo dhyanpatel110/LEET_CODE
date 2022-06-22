dbab192f0cc69803cd0a2181ea5ab0c96fc80c6f
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-06-22 23:53:12 +0530

    Update 207_Course_Schedule.py

--- python/207_Course_Schedule.py
+++ python/207_Course_Schedule.py
@@ -12,7 +12,7 @@ Explanation: There are a total of 2 courses to take.
 To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
 '''
 
-CODE:
+#CODE:
   class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         #each course to prereqisite list
