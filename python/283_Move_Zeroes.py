45ec502224e889f9e54bad41addb71f0057a5981
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-08-03 22:20:19 +0530

    Easy

---
+++ python/283_Move_Zeroes.py
@@ -0,0 +1,25 @@
+'''
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+'''
+
+#CODE:
+  class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        for right in range(len(nums)):
+            if nums[right]:
+                nums[left], nums[right] = nums[right],  nums[left]
+                left += 1
+        return nums
+            
+  
+  
