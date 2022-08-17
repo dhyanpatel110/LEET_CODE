28883099cfec645461a8c14192a7fa6380171279
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-08-17 22:32:22 +0530

    Hard

---
+++ python/295_Find_Median_from_Data_Stream.py
@@ -0,0 +1,59 @@
+'''
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+'''
+
+#CODE:
+  class MedianFinder:
+
+    def __init__(self):
+        # two heaps, large, small, minheap, maxheap
+        # heaps should be equal size
+        self.small = []
+        self.large = []
+        
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small, -1 * num)
+        
+        # make sure every num small is <= every num in large
+        if (self.small and self.large and (-1 * self.small[0]) > self.large[0]):
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        
+        # uneven size?
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+            
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        if len(self.large) > len(self.small):
+            return self.large[0]
+        
+        return (-1 * self.small[0] + self.large[0]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+
+
