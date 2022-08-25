876a5c0b13892f9ea4ead245861eaacb9dc907e5
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-08-25 23:17:06 +0530

    Easy

---
+++ python/338_Counting_Bits.py
@@ -0,0 +1,35 @@
+'''
+Example 1:
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+
+Example 2:
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+'''
+
+#CODE:
+  class Solution(object):
+    def countBits(self, n):
+        dp = [0] * (n +1)
+        offset = 1
+        
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+        return dp
+        
+        
+        
