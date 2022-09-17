277c08dad39f261cb3f1ac6af2ed72297cf57d99
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-09-17 22:21:17 +0530

    Medium

---
+++ python/319_Bulb_Switcher.py
@@ -0,0 +1,18 @@
+'''
+Example 1:
+Input: n = 3
+Output: 1
+Explanation: At first, the three bulbs are [off, off, off].
+After the first round, the three bulbs are [on, on, on].
+After the second round, the three bulbs are [on, off, on].
+After the third round, the three bulbs are [on, off, off]. 
+So you should return 1 because there is only one bulb is on.
+'''
+
+#CODE:
+  class Solution(object):
+    def bulbSwitch(self, n):
+        return int(math.sqrt(n))
+        
+        
+        
