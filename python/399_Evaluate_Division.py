d7654255497d0a3eef5d504ffd62f52722d764d3
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-10-20 23:56:40 +0530

    Medium

---
+++ python/399_Evaluate_Division.py
@@ -0,0 +1,67 @@
+'''
+-> You are given an array of variable pairs equations and an array of real numbers values, 
+where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. 
+Each Ai or Bi is a string that represents a single variable.
+
+-> You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query where you must find the answer for Cj / Dj = ?.
+
+-> Return the answers to all queries. If a single answer cannot be determined, return -1.0.
+
+
+Example 1:
+Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+Explanation: 
+Given: a / b = 2.0, b / c = 3.0
+queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+
+Example 2:
+Input: equations = [["a","b"],["b","c"],["bc","cd"]], values = [1.5,2.5,5.0], queries = [["a","c"],["c","b"],["bc","cd"],["cd","bc"]]
+Output: [3.75000,0.40000,5.00000,0.20000]
+
+Example 3:
+Input: equations = [["a","b"]], values = [0.5], queries = [["a","b"],["b","a"],["a","c"],["x","y"]]
+Output: [0.50000,2.00000,-1.00000,-1.00000]
+'''
+
+#CODE:
+  class Solution(object):
+    def calcEquation(self, equations, values, queries):
+        """
+        :type equations: List[List[str]]
+        :type values: List[float]
+        :type queries: List[List[str]]
+        :rtype: List[float]
+        """
+        # step 1: Build the Graph
+        graph = collections.defaultdict(dict)
+        for (x,y), val in zip(equations, values):
+            graph[x][y] = val
+            graph[y][x] = 1.0 / val
+
+        # step 2: DFS Function
+        def  dfs(x, y, visited):
+            if x not in graph or y not in graph:
+                return -1.0
+
+            if y in graph[x]:
+                return graph[x][y]
+            
+            for i in graph[x]:
+                if i not in visited:
+                    visited.add(i)
+                    temp = dfs(i, y, visited)
+                    if temp == -1:
+                        continue
+                    else:
+                        return graph[x][i] * temp
+            return -1
+
+        res = []
+        for query in queries:
+            res.append(dfs(query[0], query[1], set()))
+        return res
+
+      
+      
